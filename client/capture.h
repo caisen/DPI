be6e3d06a218f778b94a2e7fe794a974abb1d063
ligq <liguoqiang@gmail.com>
2015-07-08 12:11:34 +0800

    修复编译错误
    gcc -c -o capture.o -Wall -g -O0 capture.c
    capture.c: In function 'core':
    capture.c:104: error: too few arguments to function 'dcenter_packet'
    capture.c: At top level:
    capture.c:110: error: conflicting types for 'dcenter_packet'
    capture.h:10: note: previous declaration of 'dcenter_packet' was here
    capture.c: In function 'zsend':
    capture.c:142: warning: pointer targets in passing argument 1 of
    'compress2' differ in signedness
    /usr/include/zlib.h:1024: note: expected 'Bytef *' but argument is of
    type 'char *'
    capture.c:142: warning: pointer targets in passing argument 3 of
    'compress2' differ in signedness
    /usr/include/zlib.h:1024: note: expected 'const Bytef *' but argument is
    of type 'char *'
    capture.c:146: warning: format '%-6d' expects type 'int', but argument 3
    has type 'long unsigned int

--- client/capture.h
+++ client/capture.h
@@ -7,7 +7,7 @@
 
 void* zsend(void* ptr);
 int capture(const char* interface, const char* filter);
-void dcenter_packet(http_request_t* req, char* cookie, unsigned int ip_id, int sport);
+void dcenter_packet(http_request_t* req);
 void core(u_char *param, const struct pcap_pkthdr *pkthdr, const u_char *raw_data);
 
 #endif /* __CAPTURE_H__ */
